28cad8a311ce2e84f92416f2dae825de2334682b
ledangquangdangquang <quang.ld224113@sis.hust.edu.vn>
2025-07-12 00:40:25 +0700

    alias nixos-rebuild home-manager switch

--- home-manager/modules/bash.nix
+++ home-manager/modules/bash.nix
@@ -1,8 +1,12 @@
 {
 	programs.bash = {
-	enable = true;
-	initExtra = ''
-		export EDITOR=nvim
-	'';
+		enable = true;
+		initExtra = ''
+			export EDITOR=nvim
+		'';
+		shellAliases = {
+			home-rebuild = "home-manager switch --flake ./";
+			nixos-rebuild = "nixos-rebuild switch --flake ./";
+		};
 	};
 }
