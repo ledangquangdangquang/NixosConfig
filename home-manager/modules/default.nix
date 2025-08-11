ad6a5361d8d7cbda5aafe1d48da5842cee4e1f89
ledangquangdangquang <quang.ld224113@sis.hust.edu.vn>
2025-08-11 23:28:24 +0700

    kitty

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -2,6 +2,7 @@
   imports = [
     # --- File ---
     ./alacritty.nix
+    ./kitty.nix
     ./autorandr.nix
     ./bluetooth.nix
     ./btop.nix
---
+++ home-manager/modules/kitty.nix
@@ -0,0 +1,13 @@
+{
+  catppuccin.kitty = {
+    enable = true;
+    flavor = "mocha";
+  };
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "FiraCode Nerd Font Mono";
+      size = 14;
+    };
+  };
+}
